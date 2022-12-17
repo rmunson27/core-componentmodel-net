6a6e20b9ff741bcb122f701521c7efd69ec401ed
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-12-17 00:43:21 -0500

    Added `Try` class
    This class contains helper methods for writing standard "Try" methods with a single expression.

---
+++ CoreComponentModel/CoreComponentModel/Try.cs
@@ -0,0 +1,173 @@
+﻿using Rem.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.ComponentModel;
+
+/// <summary>
+///     Static helper methods for standard "Try" methods.
+/// </summary>
+/// 
+/// <remarks>
+///     The methods in this class can be used to write a standard "Try" method as a single expression.
+///     <para/>
+///     
+///     For example, consider the following (rather contrived) example:
+///     <code>
+///         public bool TryGetPositiveString(int i, [MaybeNullWhen(false)] out string s)
+///             => i > 0 ? Try.Success(out s, i.ToString()) : Try.Failure(out s);
+///     </code>
+/// 
+///     as opposed to the more verbose standard equivalent:
+///     <code>
+///         public bool TryGetPositiveString(int i, [MaybeNullWhen(false)] out string s)
+///         {
+///             if (i > 0)
+///             {
+///                 s = i.ToString();
+///                 return true;
+///             }
+///             else
+///             {
+///                 s = default;
+///                 return false;
+///             }
+///         }
+///     </code>
+/// </remarks>
+public static class Try
+{
+    #region Success Setters
+    /// <summary>
+    /// Sets <paramref name="successOut"/> to <paramref name="successValue"/> and <paramref name="failureOut"/> to
+    /// <see langword="default"/> and returns <see langword="true"/>.
+    /// </summary>
+    /// <typeparam name="TSuccess">The type of <paramref name="successOut"/>.</typeparam>
+    /// <typeparam name="TFailure">The type of <paramref name="failureOut"/>.</typeparam>
+    /// <param name="successOut">
+    /// The <see langword="out"/> parameter to set to <paramref name="successValue"/>.
+    /// </param>
+    /// <param name="successValue">The value to set <paramref name="successOut"/> to.</param>
+    /// <param name="failureOut">
+    /// The <see langword="out"/> parameter to set to <see langword="default"/>.
+    /// </param>
+    /// <returns><see langword="true"/></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool Success<TSuccess, TFailure>(out TSuccess successOut, TSuccess successValue,
+                                                   [MaybeNull, MaybeDefault] out TFailure failureOut)
+        => Success(out successOut, successValue, out failureOut, failureValue: default);
+
+    /// <summary>
+    /// Sets <paramref name="successOut"/> to <paramref name="successValue"/> and <paramref name="failureOut"/> to
+    /// <paramref name="failureValue"/> and returns <see langword="true"/>.
+    /// </summary>
+    /// <typeparam name="TSuccess">The type of <paramref name="successOut"/>.</typeparam>
+    /// <typeparam name="TFailure">The type of <paramref name="failureOut"/>.</typeparam>
+    /// <param name="successOut">
+    /// The <see langword="out"/> parameter to set to <paramref name="successValue"/>.
+    /// </param>
+    /// <param name="successValue">The value to set <paramref name="successOut"/> to.</param>
+    /// <param name="failureOut">
+    /// The <see langword="out"/> parameter to set to <paramref name="failureValue"/>.
+    /// </param>
+    /// <param name="failureValue">The value to set <paramref name="failureOut"/> to.</param>
+    /// <returns><see langword="true"/></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool Success<TSuccess, TFailure>(out TSuccess successOut, TSuccess successValue,
+                                                   [MaybeNull, MaybeDefault] out TFailure failureOut,
+                                                   [AllowNull, AllowDefault] TFailure failureValue)
+    {
+        successOut = successValue;
+        failureOut = failureValue;
+        return true;
+    }
+
+    /// <summary>
+    /// Sets <paramref name="outParameter"/> to <paramref name="value"/> and returns <see langword="true"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of <paramref name="outParameter"/>.</typeparam>
+    /// <param name="value">The value to set <paramref name="outParameter"/> to.</param>
+    /// <param name="outParameter">The <see langword="out"/> parameter to set to <paramref name="value"/>.</param>
+    /// <returns><see langword="true"/></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool Success<T>(out T outParameter, T value)
+    {
+        outParameter = value;
+        return true;
+    }
+    #endregion
+
+    #region Failure Setters
+    /// <summary>
+    /// Sets <paramref name="successOut"/> to <see langword="default"/> and <paramref name="failureOut"/> to
+    /// <paramref name="failureValue"/> and returns <see langword="false"/>.
+    /// </summary>
+    /// <typeparam name="TSuccess">The type of <paramref name="successOut"/>.</typeparam>
+    /// <typeparam name="TFailure">The type of <paramref name="failureOut"/>.</typeparam>
+    /// <param name="successOut">
+    /// The <see langword="out"/> parameter to set to <see langword="default"/>.
+    /// </param>
+    /// <param name="failureOut">
+    /// The <see langword="out"/> parameter to set to <paramref name="failureValue"/>.
+    /// </param>
+    /// <param name="failureValue">The value to set <paramref name="failureOut"/> to.</param>
+    /// <returns><see langword="false"/></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool Failure<TSuccess, TFailure>([MaybeNull, MaybeDefault] out TSuccess successOut,
+                                                   out TFailure failureOut, TFailure failureValue)
+        => Failure(out successOut, successValue: default, out failureOut, failureValue);
+
+    /// <summary>
+    /// Sets <paramref name="successOut"/> to <paramref name="successValue"/> and <paramref name="failureOut"/> to
+    /// <paramref name="failureValue"/> and returns <see langword="false"/>.
+    /// </summary>
+    /// <typeparam name="TSuccess">The type of <paramref name="successOut"/>.</typeparam>
+    /// <typeparam name="TFailure">The type of <paramref name="failureOut"/>.</typeparam>
+    /// <param name="successOut">
+    /// The <see langword="out"/> parameter to set to <paramref name="successValue"/>.
+    /// </param>
+    /// <param name="successValue">The value to set <paramref name="successOut"/> to.</param>
+    /// <param name="failureOut">
+    /// The <see langword="out"/> parameter to set to <paramref name="failureValue"/>.
+    /// </param>
+    /// <param name="failureValue">The value to set <paramref name="failureOut"/> to.</param>
+    /// <returns><see langword="false"/></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool Failure<TSuccess, TFailure>([MaybeNull, MaybeDefault] out TSuccess successOut,
+                                                   [AllowNull, AllowDefault] TSuccess successValue,
+                                                   out TFailure failureOut, TFailure failureValue)
+    {
+        successOut = successValue;
+        failureOut = failureValue;
+        return false;
+    }
+
+    /// <summary>
+    /// Sets <paramref name="outParameter"/> to <see langword="default"/> and returns <see langword="false"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of <paramref name="outParameter"/>.</typeparam>
+    /// <param name="outParameter">The <see langword="out"/> parameter to set to <see langword="default"/>.</param>
+    /// <returns><see langword="false"/></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool Failure<T>([MaybeNull, MaybeDefault] out T outParameter) => Failure(out outParameter, default);
+
+    /// <summary>
+    /// Sets <paramref name="outParameter"/> to <paramref name="value"/> and returns <see langword="false"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of <paramref name="outParameter"/>.</typeparam>
+    /// <param name="outParameter">The <see langword="out"/> parameter to set to <paramref name="value"/>.</param>
+    /// <param name="value">The value to set <paramref name="outParameter"/> to.</param>
+    /// <returns><see langword="false"/></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool Failure<T>([MaybeNull, MaybeDefault] out T outParameter, [AllowNull, AllowDefault] T value)
+    {
+        outParameter = value;
+        return false;
+    }
+    #endregion
+}
---
+++ CoreComponentModel/CoreComponentModelTest/TryTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.CoreTest.ComponentModel;
+
+/// <summary>
+/// Tests of the <see cref="Try"/> class functionality.
+/// </summary>
+[TestClass]
+public class TryTest
+{
+    /// <summary>
+    /// Tests the set-return combo methods of the <see cref="Try"/> class that can be used to implement standard
+    /// "Try" methods.
+    /// </summary>
+    [TestMethod]
+    public void TestSetReturnCombo()
+    {
+        var wrappedArray = new[] { 0, 1, 2, 3 };
+        var wrappedString = "A String";
+        ArrayOrString array = new(wrappedArray);
+        ArrayOrString str = new(wrappedString);
+
+#pragma warning disable IDE0018 // Cleaner to declare variables here for symmetry of the assertions
+        int[]? outArray;
+        string? outString;
+#pragma warning restore IDE0018
+
+        Assert.IsTrue(array.TryGetArray(out outArray));
+        Assert.AreEqual(wrappedArray, outArray);
+
+        Assert.IsTrue(array.TryGetArray(out outArray, out outString));
+        Assert.AreEqual(wrappedArray, outArray);
+        Assert.IsNull(outString);
+
+        Assert.IsFalse(str.TryGetArray(out outArray));
+        Assert.IsNull(outArray);
+
+        Assert.IsFalse(str.TryGetArray(out outArray, out outString));
+        Assert.IsNull(outArray);
+        Assert.AreEqual(wrappedString, outString);
+    }
+
+    /// <summary>
+    /// Used for testing the <see cref="Try"/> class methods.
+    /// </summary>
+    private sealed class ArrayOrString
+    {
+        public int[]? Array { get; }
+        public string? String { get; }
+
+        public ArrayOrString(int[] Array)
+        {
+            this.Array = Array ?? throw new ArgumentNullException(nameof(Array));
+        }
+
+        public ArrayOrString(string String)
+        {
+            this.String = String ?? throw new ArgumentNullException(nameof(String));
+        }
+
+        public bool TryGetArray([MaybeNullWhen(false)] out int[] Array)
+            => this.Array is null ? Try.Failure(out Array) : Try.Success(out Array, this.Array);
+
+        public bool TryGetArray([MaybeNullWhen(true)] out int[] Array, [MaybeNullWhen(false)] out string String)
+            => this.Array is null
+                ? Try.Failure(out Array, out String, this.String)
+                : Try.Success(out Array, this.Array, out String);
+    }
+}
