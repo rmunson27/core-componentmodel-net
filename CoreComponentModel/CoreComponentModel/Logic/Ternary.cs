373a2ddcdcba920359203720988c326ef1a95756
rmunson27 <rmunson2.7@gmail.com>
2023-02-03 20:30:15 -0500

    Added a `Ternary` struct
    This can be used to supply ternary logic values to attributes, where `bool?` values are not allowed.

---
+++ CoreComponentModel/CoreComponentModel/Logic/Ternary.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Rem.Core.Attributes;
+
+namespace Rem.Core.ComponentModel.Logic;
+
+/// <summary>
+/// Represents ternary logical values, either <see langword="true"/>, <see langword="false"/>
+/// or an unknown.
+/// </summary>
+/// <remarks>
+/// This struct uses a backing enum, which can be used to pass ternary values to attribute constructors that would not
+/// allow either <see cref="Ternary"/> values or nullable <see cref="bool"/> values.
+/// The backing enum is implicitly convertible to <see cref="Ternary"/>.  The conversion will map all unnamed enum
+/// values to <see cref="Unknown"/>.
+/// </remarks>
+public readonly record struct Ternary
+{
+    /// <inheritdoc cref="Values.False"/>
+    public static readonly Ternary False = new(Values.False);
+
+    /// <inheritdoc cref="Values.Unknown"/>
+    public static readonly Ternary Unknown = new(Values.Unknown);
+
+    /// <inheritdoc cref="Values.True"/>
+    public static readonly Ternary True = new(Values.True);
+
+    #region Properties
+    /// <summary>
+    /// Determines if this instance is <see langword="true"/>.
+    /// </summary>
+    public bool IsTrue => Value == Values.True;
+
+    /// <summary>
+    /// Determines if this instance is <see langword="false"/>.
+    /// </summary>
+    public bool IsFalse => Value == Values.False;
+
+    /// <summary>
+    /// Determines if this instance is <see cref="Unknown"/>.
+    /// </summary>
+    public bool IsUnknown => Value == Values.Unknown;
+
+    /// <summary>
+    /// Gets an <see langword="enum"/> value uniquely representing this instance.
+    /// </summary>
+    public Values Value { get; }
+    #endregion
+
+    /// <summary>
+    /// Constructs this struct with the value passed in.
+    /// </summary>
+    /// <param name="Value"></param>
+    private Ternary([NameableEnum] Values Value)
+    {
+        this.Value = Value;
+    }
+
+    #region Equality
+    /// <summary>
+    /// Determines if this instance is equal to another instance of the same type.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public bool Equals(Ternary other) => Value == other.Value;
+
+    /// <summary>
+    /// Gets a hash code for this instance.
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode() => (int)Value;
+    #endregion
+
+    /// <summary>
+    /// Gets a string representation of this instance.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => IsBool(out var b) ? b.ToString() : "Unknown";
+
+    #region Logic
+    /// <summary>
+    /// Computes the logical conjunction of the two values passed in.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static Ternary operator &(Ternary left, Ternary right) => new(left.Value & right.Value);
+
+    /// <summary>
+    /// Computes the logical disjunction of the two values passed in.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static Ternary operator |(Ternary left, Ternary right) => new(left.Value | right.Value);
+
+    /// <summary>
+    /// Computes the logical exclusive disjunction of the two values passed in.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static Ternary operator ^(Ternary left, Ternary right)
+        => left.IsBool(out var leftB) && right.IsBool(out var rightB)
+            ? leftB ^ rightB
+            : Unknown; // Any unknown value creates an unknown
+    #endregion
+
+    #region Conversions
+    /// <summary>
+    /// Implicitly converts a <see cref="Ternary"/> to a <see cref="Values"/>.
+    /// </summary>
+    /// <param name="t"></param>
+    public static implicit operator Values(Ternary t) => t.Value;
+
+    /// <summary>
+    /// Implicitly converts a <see cref="Values"/> to a <see cref="Ternary"/>.
+    /// </summary>
+    /// <remarks>
+    /// This method will return <see cref="Unknown"/> for any unnamed <see cref="Values"/> instance.
+    /// </remarks>
+    /// <param name="value"></param>
+    public static implicit operator Ternary(Values value) => value switch
+    {
+        Values.False => False,
+        Values.True => True,
+        _ => Unknown,
+    };
+
+    /// <summary>
+    /// Implicitly converts a nullable <see cref="bool"/> to a <see cref="Ternary"/>.
+    /// </summary>
+    /// <param name="value"></param>
+    public static implicit operator Ternary(bool? value) => value is bool b ? b : Unknown;
+
+    /// <summary>
+    /// Implicitly converts a <see cref="Ternary"/> to a nullable <see cref="bool"/>.
+    /// </summary>
+    /// <param name="t"></param>
+    public static implicit operator bool?(Ternary t) => t.Value == Unknown ? null : t.Value != Values.False;
+
+    /// <summary>
+    /// Implicitly converts a <see cref="bool"/> to a <see cref="Ternary"/>.
+    /// </summary>
+    /// <param name="value"></param>
+    public static implicit operator Ternary(bool value) => value ? True : False;
+
+    /// <summary>
+    /// Determines if this instance represents a boolean value, setting the value in an <see langword="out"/>
+    /// parameter if so.
+    /// </summary>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public bool IsBool(out bool b) => IsUnknown ? Try.Failure(out b) : Try.Success(out b, Value != Values.False);
+
+    /// <summary>
+    /// Determines if this instance represents a boolean value.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsBool() => !IsUnknown;
+    #endregion
+
+    /// <summary>
+    /// Represents all values of the <see cref="Ternary"/> type as <see langword="enum"/> values.
+    /// </summary>
+    public enum Values : byte
+    { 
+        /// <summary>
+        /// Represents <see langword="false"/>.
+        /// </summary>
+        False = 0,
+
+        /// <summary>
+        /// Represents an unknown.
+        /// </summary>
+        Unknown = 135, // Closest possible to 128 with half of bits set
+
+        /// <summary>
+        /// Represents <see langword="true"/>.
+        /// </summary>
+        True = 255,
+    }
+}
---
+++ CoreComponentModel/CoreComponentModelTest/Logic/TernaryTest.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Immutable;
+using Rem.Core.ComponentModel.Logic;
+
+namespace Rem.CoreTest.ComponentModel.Logic;
+
+/// <summary>
+/// Tests of the <see cref="Ternary"/> struct.
+/// </summary>
+[TestClass]
+public class TernaryTest
+{
+    /// <summary>
+    /// The <see cref="Ternary.Unknown"/> value.
+    /// </summary>
+    private static readonly Ternary Unknown = Ternary.Unknown;
+
+    /// <summary>
+    /// Contains all values of <see cref="Ternary"/>.
+    /// </summary>
+    private static readonly ImmutableArray<Ternary> All
+        = ImmutableArray.CreateRange(new Ternary[] { true, false, Unknown });
+
+    /// <summary>
+    /// Tests the conjunction (&) operator.
+    /// </summary>
+    [TestMethod]
+    public void TestAnd()
+    {
+        var table = new TruthTable
+        {
+            True = new(True: true, False: false, Unknown: Unknown),
+            False = new(True: false, False: false, Unknown: false),
+            Unknown = new(True: Unknown, False: false, Unknown: Unknown),
+        };
+
+        foreach (var left in All)
+        { 
+            foreach (var right in All)
+            {
+                Assert.AreEqual(table[left][right], left & right, $"{left} & {right}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Tests the disjunction (|) operator.
+    /// </summary>
+    [TestMethod]
+    public void TestOr()
+    {
+        var table = new TruthTable
+        {
+            True = new(True: true, False: true, Unknown: true),
+            False = new(True: true, False: false, Unknown: Unknown),
+            Unknown = new(True: true, False: Unknown, Unknown: Unknown),
+        };
+
+        foreach (var left in All)
+        { 
+            foreach (var right in All)
+            {
+                Assert.AreEqual(table[left][right], left | right, $"{left} | {right}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Tests the exclusive disjunction (^) operator.
+    /// </summary>
+    [TestMethod]
+    public void TestXOr()
+    {
+        var table = new TruthTable
+        {
+            True = new(True: false, False: true, Unknown: Unknown),
+            False = new(True: true, False: false, Unknown: Unknown),
+            Unknown = new(True: Unknown, False: Unknown, Unknown: Unknown),
+        };
+
+        foreach (var left in All)
+        { 
+            foreach (var right in All)
+            {
+                Assert.AreEqual(table[left][right], left ^ right, $"{left} ^ {right}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents a truth table.
+    /// </summary>
+    private sealed class TruthTable
+    { 
+        /// <summary>
+        /// Gets the row of the truth table for <see cref="Ternary.True"/> on the left-hand side.
+        /// </summary>
+        public Row True { get; init; }
+
+        /// <summary>
+        /// Gets the row of the truth table for <see cref="Ternary.False"/> on the left-hand side.
+        /// </summary>
+        public Row False { get; init; }
+
+        /// <summary>
+        /// Gets the row of the truth table for <see cref="Ternary.Unknown"/> on the left-hand side.
+        /// </summary>
+        public Row Unknown { get; init; }
+
+        /// <summary>
+        /// Gets the row representing the result of applying the operation under test to <paramref name="left"/> and
+        /// other values of <see cref="Ternary"/>.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <returns></returns>
+        public Row this[Ternary left] => left.Value switch
+        { 
+            Ternary.Values.True => True,
+            Ternary.Values.False => False,
+            _ => Unknown,
+        };
+
+        /// <summary>
+        /// Represents a row of a truth table.
+        /// </summary>
+        public readonly struct Row
+        {
+            /// <summary>
+            /// Gets the expected result of applying the operator under test to the value under test
+            /// and <see cref="Ternary.True"/>.
+            /// </summary>
+            public Ternary True { get; }
+
+            /// <summary>
+            /// Gets the expected result of applying the operator under test to the value under test
+            /// and <see cref="Ternary.False"/>.
+            /// </summary>
+            public Ternary False { get; }
+
+            /// <summary>
+            /// Gets the expected result of applying the operator under test to the value under test
+            /// and <see cref="Ternary.Unknown"/>.
+            /// </summary>
+            public Ternary Unknown { get; }
+
+            /// <summary>
+            /// Gets the result of the operator under test applied to the <see cref="Ternary"/> this row represents
+            /// results for and <paramref name="right"/>.
+            /// </summary>
+            /// <param name="right"></param>
+            /// <returns></returns>
+            public Ternary this[Ternary right] => right.Value switch
+            {
+                Ternary.Values.False => False,
+                Ternary.Values.True => True,
+                _ => Unknown,
+            };
+
+            /// <summary>
+            /// Constructs a row of the truth table with the expected results.
+            /// </summary>
+            /// <param name="True"></param>
+            /// <param name="False"></param>
+            /// <param name="Unknown"></param>
+            public Row(Ternary True, Ternary False, Ternary Unknown)
+            {
+                this.True = True;
+                this.False = False;
+                this.Unknown = Unknown;
+            }
+        }
+    }
+}
