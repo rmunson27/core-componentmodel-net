41cf6103533ec3692909d21725ae7a51d3c8c10f
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2023-04-11 01:42:13 -0400

    Added exception for defaultable `struct` values being the default
    This exception is an analog of `NullReferenceException`, as `StructArgumentDefaultException` is an analog of `ArgumentNullException`.

---
+++ CoreComponentModel/CoreComponentModel/StructDefaultException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.ComponentModel;
+
+/// <summary>
+/// An exception thrown when a <see langword="struct"/> value is its invalid default value.
+/// </summary>
+/// <remarks>
+/// This exception can be considered an analog of the <see cref="NullReferenceException"/> for defaultable
+/// <see langword="struct"/> values.
+/// </remarks>
+public class StructDefaultException : Exception
+{
+    /// <summary>
+    /// Constructs a new instance of the <see cref="StructDefaultException"/> class with a default error message.
+    /// </summary>
+    public StructDefaultException() : base("Value was default.") { }
+
+    /// <summary>
+    /// Constructs a new instance of the <see cref="StructDefaultException"/> class with the specified error message.
+    /// </summary>
+    /// <param name="message"></param>
+    public StructDefaultException(string message) : base(message) { }
+
+    /// <summary>
+    /// Constructs a new instance of the <see cref="StructDefaultException"/> class with the specified error message
+    /// and inner exception.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public StructDefaultException(string message, Exception? innerException) : base(message, innerException) { }
+
+    /// <summary>
+    /// Constructs a new instance of the <see cref="StructDefaultException"/> class from the serialization data passed
+    /// in (serialization constructor).
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="context"></param>
+    protected StructDefaultException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
