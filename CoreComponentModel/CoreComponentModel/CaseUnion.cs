884f831221dfe92ff69b2e9f319f06cd669e7381
rmunson27 <rmunson2.7@gmail.com>
2023-04-20 03:47:26 -0400

    Corrected some typos in doc comments in "CaseUnion.cs"

--- CoreComponentModel/CoreComponentModel/CaseUnion.cs
+++ CoreComponentModel/CoreComponentModel/CaseUnion.cs
@@ -6,18 +6,18 @@ using Rem.Core.Attributes;
 namespace Rem.Core.ComponentModel;
 
 /// <summary>
-/// Static functionality for implementors of the <see cref="IEnumeratedCaseUnion{TSelf, TCases}"/> interface.
+/// Static functionality for case union types.
 /// </summary>
 /// <remarks>
-/// Some of this class consists of helper functionality that may help dependent projects implement the interface.
+/// Some of this class consists of helper functionality that may help dependent projects implement such types.
 /// </remarks>
 public static class CaseUnion
 {
     /// <summary>
     /// Creates an appropriate exception that can be thrown to indicate that an <see langword="enum"/> value
-    /// representing a case of a type implementing <see cref="IEnumeratedCaseUnion{TSelf, TCases}"/> is not defined.
+    /// representing a case of a case union type is not defined.
     /// </summary>
-    /// <typeparam name="TSelf">The type that implements <see cref="IEnumeratedCaseUnion{TSelf, TCases}"/>.</typeparam>
+    /// <typeparam name="TSelf">The case union type.</typeparam>
     /// <returns>
     /// An <see cref="InvalidEnumArgumentException"/> with an appropriate message describing the exceptional case.
     /// </returns>
@@ -26,16 +26,7 @@ public static class CaseUnion
         => new($"Invalid {typeof(TSelf)} enumeration case.");
 
     /// <summary>
-    /// Indicates that the targeted <see langword="enum"/> definition nested within a type provides the case-backing
-    /// enumeration for the type.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Enum,
-                    AllowMultiple = false,
-                    Inherited = false)]
-    public sealed class CasesAttribute : Attribute { }
-
-    /// <summary>
-    /// Marks the constructor of a type decorated with <see cref="CaseUnionAttribute"/> that should be used to
+    /// Marks the constructor of a type decorated with <see cref="CaseUnionAttribute{TCases}"/> that should be used to
     /// construct instances of the type.
     /// </summary>
     /// <remarks>
@@ -47,19 +38,19 @@ public static class CaseUnion
     public sealed class ConstructorAttribute : Attribute { }
 
     /// <summary>
-    /// Decorates the converter from a type decorated with <see cref="CaseUnionAttribute"/> to the underlying
+    /// Decorates the converter from a type decorated with <see cref="CaseUnionAttribute{TCases}"/> to the underlying
     /// <see langword="enum"/> case.
     /// </summary>
     public sealed class ToCaseCastAttribute : CaseCastAttribute { }
 
     /// <summary>
-    /// Decorates the converter from a type decorated with <see cref="CaseUnionAttribute"/> to the underlying
+    /// Decorates the converter from a type decorated with <see cref="CaseUnionAttribute{TCases}"/> to the underlying
     /// <see langword="enum"/> case.
     /// </summary>
     public sealed class FromCaseCastAttribute : CaseCastAttribute { }
 
     /// <summary>
-    /// Decorates a conversion operator between a type decorated with <see cref="CaseUnionAttribute"/> and its
+    /// Decorates a conversion operator between a type decorated with <see cref="CaseUnionAttribute{TCases}"/> and its
     /// underlying <see langword="enum"/> case type.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method,
@@ -71,11 +62,11 @@ public static class CaseUnion
     }
 
     /// <summary>
-    /// Decorates the instance property of a type decorated with <see cref="CaseUnionAttribute"/> that stores the case
-    /// of the instance.
+    /// Decorates the instance property of a type decorated with <see cref="CaseUnionAttribute{TCases}"/> that stores
+    /// the case of the instance.
     /// </summary>
     /// <remarks>
-    /// This property should occur exactly once within a type decorated with <see cref="CaseUnionAttribute"/>.
+    /// This property should occur exactly once within a type decorated with <see cref="CaseUnionAttribute{TCases}"/>.
     /// </remarks>
     [AttributeUsage(AttributeTargets.Property,
                     AllowMultiple = false,
