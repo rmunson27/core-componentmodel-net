676963530314f8f9bbb2697d1309d4c94f3cbf1f
rmunson27 <rmunson2.7@gmail.com>
2023-04-19 22:39:44 -0400

    Refactored `StructArgumentDefaultException` to extend `ArgumentNullException`
    This is essentially the `struct` version of `ArgumentNullException`, so this change seems appropriate rather than forcing users to handle the exceptions separately.

---
+++ CoreComponentModel/CoreComponentModel/CaseUnion.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Rem.Core.Attributes;
+
+namespace Rem.Core.ComponentModel;
+
+/// <summary>
+/// Static functionality for implementors of the <see cref="IEnumeratedCaseUnion{TSelf, TCases}"/> interface.
+/// </summary>
+/// <remarks>
+/// Some of this class consists of helper functionality that may help dependent projects implement the interface.
+/// </remarks>
+public static class CaseUnion
+{
+    /// <summary>
+    /// Creates an appropriate exception that can be thrown to indicate that an <see langword="enum"/> value
+    /// representing a case of a type implementing <see cref="IEnumeratedCaseUnion{TSelf, TCases}"/> is not defined.
+    /// </summary>
+    /// <typeparam name="TSelf">The type that implements <see cref="IEnumeratedCaseUnion{TSelf, TCases}"/>.</typeparam>
+    /// <returns>
+    /// An <see cref="InvalidEnumArgumentException"/> with an appropriate message describing the exceptional case.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static InvalidEnumArgumentException InvalidCase<TSelf>()
+        => new($"Invalid {typeof(TSelf)} enumeration case.");
+
+    /// <summary>
+    /// Indicates that the targeted <see langword="enum"/> definition nested within a type provides the case-backing
+    /// enumeration for the type.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Enum,
+                    AllowMultiple = false,
+                    Inherited = false)]
+    public sealed class CasesAttribute : Attribute { }
+
+    /// <summary>
+    /// Marks the constructor of a type decorated with <see cref="CaseUnionAttribute"/> that should be used to
+    /// construct instances of the type.
+    /// </summary>
+    /// <remarks>
+    /// Exactly one <see cref="ConstructorAttribute"/> should apply to a case union type (and no more).
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Class | AttributeTargets.Struct,
+                    AllowMultiple = false,
+                    Inherited = false)]
+    public sealed class ConstructorAttribute : Attribute { }
+
+    /// <summary>
+    /// Decorates the converter from a type decorated with <see cref="CaseUnionAttribute"/> to the underlying
+    /// <see langword="enum"/> case.
+    /// </summary>
+    public sealed class ToCaseCastAttribute : CaseCastAttribute { }
+
+    /// <summary>
+    /// Decorates the converter from a type decorated with <see cref="CaseUnionAttribute"/> to the underlying
+    /// <see langword="enum"/> case.
+    /// </summary>
+    public sealed class FromCaseCastAttribute : CaseCastAttribute { }
+
+    /// <summary>
+    /// Decorates a conversion operator between a type decorated with <see cref="CaseUnionAttribute"/> and its
+    /// underlying <see langword="enum"/> case type.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method,
+                    AllowMultiple = false,
+                    Inherited = false)]
+    public abstract class CaseCastAttribute : Attribute
+    {
+        private protected CaseCastAttribute() { }
+    }
+
+    /// <summary>
+    /// Decorates the instance property of a type decorated with <see cref="CaseUnionAttribute"/> that stores the case
+    /// of the instance.
+    /// </summary>
+    /// <remarks>
+    /// This property should occur exactly once within a type decorated with <see cref="CaseUnionAttribute"/>.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Property,
+                    AllowMultiple = false,
+                    Inherited = true)]
+    public sealed class CaseAttribute : Attribute { }
+}
+
+/// <summary>
+/// Indicates that the targeted definition has an <see langword="enum"/> type nested within it that enumerates all
+/// the cases of the type.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface,
+                AllowMultiple = false,
+                Inherited = true)]
+public sealed class CaseUnionAttribute : Attribute { }
--- CoreComponentModel/CoreComponentModel/StructArgumentDefaultException.cs
+++ CoreComponentModel/CoreComponentModel/StructArgumentDefaultException.cs
@@ -11,12 +11,18 @@ namespace Rem.Core.ComponentModel;
 /// An exception thrown when a <see langword="struct"/> argument is the default value of its type when that is not
 /// permitted by the method being called.
 /// </summary>
-public class StructArgumentDefaultException : ArgumentException
+/// <remarks>
+/// This class extends <see cref="ArgumentNullException"/> because in the sense that an invalid default has zeroed bits
+/// that are invalid, it is similar to the <see cref="null"/> case, which is a zeroed reference
+/// or <see cref="Nullable{T}"/>. The hope is that this will make it easier to handle these similar exceptions, which
+/// essentially represent the same case.
+/// </remarks>
+public class StructArgumentDefaultException : ArgumentNullException
 {
     /// <summary>
     /// Constructs a new instancce of the <see cref="StructArgumentDefaultException"/> class.
     /// </summary>
-    public StructArgumentDefaultException() : base("Argument was default.") { }
+    public StructArgumentDefaultException() : base(null, "Argument was default.") { }
 
     /// <summary>
     /// Constructs a new instance of the <see cref="StructArgumentDefaultException"/> class with a default error
@@ -24,7 +30,7 @@ public class StructArgumentDefaultException : ArgumentException
     /// </summary>
     /// <param name="paramName"></param>
     public StructArgumentDefaultException(string paramName)
-        : base(FormatMessageWithParamName("Value cannot be default.", paramName))
+        : base(null, FormatMessageWithParamName("Value cannot be default.", paramName))
     { }
 
     /// <summary>
@@ -33,8 +39,8 @@ public class StructArgumentDefaultException : ArgumentException
     /// </summary>
     /// <param name="paramName"></param>
     /// <param name="message"></param>
-    public StructArgumentDefaultException(string paramName, string message)
-        : base(FormatMessageWithParamName(message, paramName))
+    public StructArgumentDefaultException(string? paramName, string? message)
+        : base(null, FormatMessageWithParamName(message, paramName))
     { }
 
     /// <summary>
@@ -51,8 +57,7 @@ public class StructArgumentDefaultException : ArgumentException
     /// </summary>
     /// <param name="info"></param>
     /// <param name="context"></param>
-    protected StructArgumentDefaultException(SerializationInfo info, StreamingContext context)
-        : base(info, context) { }
+    protected StructArgumentDefaultException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 
     /// <summary>
     /// Formats the supplied error message with the name of the parameter that caused the exception.
@@ -60,7 +65,9 @@ public class StructArgumentDefaultException : ArgumentException
     /// <param name="message"></param>
     /// <param name="paramName"></param>
     /// <returns></returns>
-    protected static string FormatMessageWithParamName(string message, string paramName)
-        => $"{message} (Parameter '{paramName}')";
+    protected static string FormatMessageWithParamName(string? message, string? paramName)
+    {
+        message ??= "Value cannot be the default.";
+        return paramName is null ? message : $"{message} (Parameter '{paramName}')";
+    }
 }
-
